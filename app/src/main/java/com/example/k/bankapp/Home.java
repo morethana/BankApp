c3f531fe11f9b1a9a321be220ae2330484823919
morethana <kkolos421@gmail.com>
2018-04-05 13:09:28 +0100

    Home

--- app/src/main/java/com/example/k/bankapp/Home.java
+++ app/src/main/java/com/example/k/bankapp/Home.java
@@ -27,128 +27,53 @@ import cz.msebera.android.httpclient.HttpResponse;
 
 public class Home extends AppCompatActivity {
 
-    private String enteredUsername;
-    private String enteredPassword;
+    private Button myFinanceButton;
+    private Button contactButton;
+    private Button infoButton;
+    private Button locationButton;
 
-    private EditText mUsername;
-    private EditText mPassword;
-    private Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_home);
 
-        mUsername = (EditText) findViewById(R.id.username);
-        mPassword = (EditText) findViewById(R.id.password);
+        myFinanceButton = (Button)findViewById(R.id.myFinanceButton);
+        contactButton = (Button)findViewById(R.id.contactButton);
+        infoButton = (Button)findViewById(R.id.infoButton);
+        locationButton = (Button)findViewById(R.id.locationButton);
 
-        mLoginButton = (Button) findViewById(R.id.loginButton);
-        mLoginButton.setOnClickListener(new View.OnClickListener() {
+        myFinanceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                buttonPressed();
+                Intent intent = new Intent(Home.this, Login.class);
+                finish();
+                startActivity(intent);
             }
         });
+
+        contactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 
-    public void  buttonPressed(){
-        enteredUsername = mUsername.getText().toString();
-        enteredPassword = mPassword.getText().toString();
-
-        Log.d("Test", enteredUsername.toString());
-        Log.d("Test", enteredPassword.toString());
-        Log.d("Test", "https://project-tobetodo.c9users.io/userLogin/" + enteredUsername + "/" + enteredPassword);
-
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="https://project-tobetodo.c9users.io/userLogin/" + enteredUsername + "/" + enteredPassword;
-        String url2 ="http://www.google.com";
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.d("Testing Volley", "Response is: "+ response.toString());
-
-                        User user = new User();
-                        user = user.fromJson(response);
-                        Log.d("TEST IN HOME", user.toString());
-                        double test = user.getAccounts().get(0).getTransactions().get(0).getAmount();
-                        Log.d("ANOTHER IN HOME", Double.toString(test));
-
-                        user.setPassword(enteredPassword);
-
-                        Intent intent = new Intent(Home.this, AccountLogin.class);
-                        intent.putExtra("User", user);
-                        finish();
-                        startActivity(intent);
-                    }
-                }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d("Testing Volley", "That didn't work!");
-
-                    }
-                });
-
-// Access the RequestQueue through your singleton class.
-        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
-
-//// Request a string response from the provided URL.
-//        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-//                new Response.Listener<String>() {
-//                    @Override
-//                    public void onResponse(String response) {
-//                        // Display the first 500 characters of the response string.
-//                        Log.d("Testing Volley", "Response is: "+ response.toString());
-//                    }
-//                }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                Log.d("Testing Volley", "That didn't work!");
-//            }
-//        });
-//
-//// Add the request to the RequestQueue.
-//        queue.add(stringRequest);
-
-//        AsyncHttpClient client = new AsyncHttpClient();
-//
-//        client.get("https://project-tobetodo.c9users.io/userLogin/" + enteredUsername + "/" + enteredPassword, new JsonHttpResponseHandler() {
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                Log.d("onSuccess", response.toString());
-//                User user = new User();
-//                user = user.fromJson(response);
-//                Log.d("TEST IN HOME", user.toString());
-//                double test = user.getAccounts().get(0).getTransactions().get(0).getAmount();
-//                Log.d("ANOTHER IN HOME", Double.toString(test));
-//
-//                user.setPassword(enteredPassword);
-//
-//                Intent intent = new Intent(Home.this, AccountLogin.class);
-//                intent.putExtra("User", user);
-//                finish();
-//                startActivity(intent);
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
-//                Log.d("onFailure", Integer.toString(statusCode));
-//                Log.d("onFailure", headers.toString());
-//                Log.d("onFailure", e.toString());
-//                Log.d("onFailure", response.toString());
-//
-//
-//            }
-//        });
-//        Log.d("Test", "Past the client method");
-//
-////        Intent intent = new Intent(this, com.example.k.bank.AccountLogin.class);
-////        finish();
-////        startActivity(intent);
-    }
 }
 
